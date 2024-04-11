d218428326704277d21400385a00f7199a34c12a
NoahBeto <154388974+NoahBeto@users.noreply.github.com>
2024-04-11 13:39:23 -0700

    Create driverProgram.java

---
+++ driverProgram.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+/**
+* The purpose of this program is to take the user input and put it through methods in another class 
+* in order to determine if the user input is a palindrome or not.
+*/
+public class driverProgram {
+    public static void main(String[] args) {
+    	palindromeChecker object = new palindromeChecker();
+        object.promptingUser();
+	}
+}
